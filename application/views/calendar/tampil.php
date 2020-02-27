4b80d9ab6e4a5d7c94fd04beed02a6593df94627
Jawaldi <jawaldi416@gmail.com>
2020-02-27 21:27:53 +0700

    Mengubah view tampil.php

--- application/views/calendar/tampil.php
+++ application/views/calendar/tampil.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Jquery FullCalendar Integration with Codeigniter using Ajax</title>
+    <title>FullCalendar Dengan Codeigniter & Ajax</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -29,7 +29,7 @@
 </head>
 <body>
     <br />
-    <h2 align="center"><a href="#">Jquery FullCalendar Integration with Codeigniter using Ajax</a></h2>
+    <h2 align="center"><a href="#">FullCalendar Dengan Codeigniter & Ajax</a></h2>
     <br />
     <div class="container">
         <div id="calendar"></div>
